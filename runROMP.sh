f451c3bdb8870598b04fb8b04d0927f96f8bbe6e
Alex Smith <alsmi14@ilstu.edu>
2021-11-07 16:08:31 -0600

    Created a script to run romp implementation

---
+++ runROMP.sh
@@ -0,0 +1,15 @@
+make cleanROMP
+make romp
+
+outFolder='Out'
+binFolder='Bin'
+outFile=$outFolder/'romp_output'
+
+for N in 1000000 10000000 100000000
+do
+    for nThreads in 1 2 5 10
+    do
+        ./$binFolder/romp_sieve $nThreads $N >> $outFile
+    done
+    echo =============== >> $outFile
+done
