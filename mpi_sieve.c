4003e57dc3197e277450a08a0df5fda201a7c507
khartman428 <k.hartman428@gmail.com>
2021-11-08 09:59:46 -0600

    initial branch commit

--- mpi_sieve.c
+++ mpi_sieve.c
@@ -1,6 +1,6 @@
 /**
  * This program parallelizes the Sieve of Eratosthenes using MPI to find all primes up to a specified natural number.
  * 
- * @author 
- * @date 
+ * @author Katelyn Hartman
+ * @date 11/8/21
  */
