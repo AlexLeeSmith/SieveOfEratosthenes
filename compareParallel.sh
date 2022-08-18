75e2404fdf924259fd7fb065f75bbb0137dfa54d
Alex Smith <alex1000242@gmail.com>
2022-08-18 00:23:08 -0500

    Fixed omp_sieve call error

--- compareParallel.sh
+++ compareParallel.sh
@@ -13,8 +13,8 @@ for N in 100000000 1000000000
 do
     for nThreads in 1 2 5 10
     do
-        ./$binFolder/omp_sieve $nThreads omp $N >> $outFile
-        ./$binFolder/omp_sieve $nThreads romp $N >> $outFile
+        ./$binFolder/omp_sieve $nThreads $N omp >> $outFile
+        ./$binFolder/omp_sieve $nThreads $N romp >> $outFile
         echo ~~~~~~~~~~~~~~~ >> $outFile
     done
     echo =============== >> $outFile
